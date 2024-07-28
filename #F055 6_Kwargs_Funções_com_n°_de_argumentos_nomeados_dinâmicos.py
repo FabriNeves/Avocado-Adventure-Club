e07533b76e2d40fb08861c3260992ced5cfe364f
FaSnow <eng.fbneves@gmail.com>
2024-07-28 18:47:40 -0300

    #f055

--- #F055 6_Kwargs_Funções_com_n°_de_argumentos_nomeados_dinâmicos.py
+++ #F055 6_Kwargs_Funções_com_n°_de_argumentos_nomeados_dinâmicos.py
@@ -9,3 +9,13 @@ def concatenar(**palavras):
 concatenar(a='Nós ' , b='Somos ', c='Pythonistas ' , d='Profissionais.')
 
 
+def fazer_calculo(nome,*args,**kwargs):
+    print(nome)
+    print(args)
+    print(kwargs)
+    for arg in args:
+        print(arg)
+    for kwargs in kwargs.values():
+        print(kwargs)
+    
+fazer_calculo('Jeff',4,6,3,7,a=1,b=2,c=3)
